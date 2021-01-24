456c619a02b10b8aaecd618947738a419c4821e3
r1ngs <r1ngs@outlook.com>
2021-01-24 17:23:33 +0800

    优化UI，增加Icon

--- app.py
+++ app.py
@@ -560,6 +560,7 @@ class mainCode(QMainWindow, mainWindowFront.Ui_MainWindow):
 
             # 更新目录列表
             # print(files)
+
             item = treeWidget.currentItem()
             childL = []
             childCount = item.childCount()
